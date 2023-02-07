57413c93403eb88a10a98a41361a4ebf438cbfe8
tnmbot1 <124488078+tnmbot1@users.noreply.github.com>
2023-02-07 13:33:24 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoRecord = true //auto recording (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
 
 //documents variants
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
@@ -14,20 +14,20 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94764497078'] //ur owner number
-global.ownername = "Sandaru Nethsara" //ur owner name
-global.ytname = "YT: Sandaru Yt" //ur yt chanel name
-global.socialm = "GitHub: Mr-Katussa" //ur github or insta name
-global.location = "Sri Lanka, Colombo, Kiribathgoda" //ur location
+global.owner = ['94761303496'] //ur owner number
+global.ownername = "Thimira nimsara" //ur owner name
+global.ytname = "YT: TnM GeTec" //ur yt chanel name
+global.socialm = "GitHub: TNMBOT" //ur github or insta name
+global.location = "Sri Lanka," //ur location
 
 //bot bomdy 
-global.ownernomer = "94764497078" //ur number
-global.premium = ['94764497078'] //ur premium number
-global.botname = 'Xcoco' //ur bot name
+global.ownernomer = "94761303496" //ur number
+global.premium = ['947613034968'] //ur premium number
+global.botname = 'TnM bot' //ur bot name
 global.themeemoji = "♻️" //ur theme emoji
-global.packname = "Sticker By Xcoco" //ur sticker watermark packname
-global.author = "Xcoco" //ur sticker watermark author
-global.wm = "XCOCO BOT INC" //ur watermark
+global.packname = "Sticker By TNM" //ur sticker watermark packname
+global.author = "Tnm" //ur sticker watermark author
+global.wm = "TNM BOT INC" //ur watermark
 
 // Other
 global.sessionName = 'session'
